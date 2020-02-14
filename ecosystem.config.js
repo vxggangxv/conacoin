d9c2dde450700f507454ea0ccc135e87e912976c
vxggangxv <vxggangxv@naver.com>
2020-02-14 17:56:20 +0900

    edit ecosystems

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,15 +1,23 @@
 module.exports = {
-    apps: [{
-        name: 'conacoin',
-        script: 'server.js',
-        instances: 3,
-        exec_mode: 'cluster',
-        wait_ready: true,
-        listen_timeout: 5000,
-        kill_timeout: 5000,
-        error_file: 'log/err.text',
-        out_file: 'log/out.text',
-        log_file: 'log/combined.text',
-        time: true
-    }]
+    apps: [
+        {
+            name: 'conacoin',
+            script: 'server.js',
+            instances: 3,
+            exec_mode: 'cluster',
+            wait_ready: true,
+            listen_timeout: 5000,
+            kill_timeout: 5000,
+            env: {
+                NODE_ENV: 'development',
+            },
+            env_production: {
+                NODE_ENV: 'production',
+            },
+            error_file: 'log/err.text',
+            out_file: 'log/out.text',
+            log_file: 'log/combined.text',
+            time: true,
+        },
+    ],
 }
